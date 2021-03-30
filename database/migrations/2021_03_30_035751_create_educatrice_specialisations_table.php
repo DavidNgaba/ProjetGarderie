d8b729c730aad918d36f6d27a4293ab9b4726b66
007nicky <007nicky@protonmail.com>
2021-03-30 00:07:34 -0400

    Migration de la table Specialisations

---
+++ database/migrations/2021_03_30_035751_create_educatrice_specialisations_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEducatriceSpecialisationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('educatrice_specialisations', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('educatrice_id')->constrained()->onDelete('cascade');
+            $table->text('description');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('educatrice_specialisations');
+    }
+}
