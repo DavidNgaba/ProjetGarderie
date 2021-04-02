b0a9d4c0fb4c8219fa1bc2e19cb4b2c39ae6ff66
David Ngaba <davidngaba.dn@gmail.com>
2021-04-02 18:41:26 -0400

    test DavidNgaba
    J'ai juste ajouter un espace dans le fichier

--- app/Models/Educatrice.php
+++ app/Models/Educatrice.php
@@ -2,7 +2,7 @@
 
 namespace App\Models;
 
-use App\Models\Enfant;
+use App\Models\Enfant; 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
